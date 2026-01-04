8cafd288c6f6d88eec117cdbde996dad0c4b019b
Deepjyoti <raydeepjyoti709@gmail.com>
2026-01-04 18:15:53 +0000

    lecture 10 done ✅

---
+++ util/random.go
@@ -0,0 +1,42 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	const letters = "abcdefghijklmnopqrstuvwxyz"
+
+	var sb strings.Builder
+	k := len(letters)
+
+	for i := 0; i < n; i++ {
+		sb.WriteByte(letters[rand.Intn(k)])
+	}
+
+	return sb.String()
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "INR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
